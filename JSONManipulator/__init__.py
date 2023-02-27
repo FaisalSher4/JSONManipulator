4706548d852ebcf95a77130d8d4044c53ec9ca1b
FaisalSher4 <75548232+FaisalSher4@users.noreply.github.com>
2023-02-27 23:58:24 +0500

    Update __init__.py

--- JSONManipulator/__init__.py
+++ JSONManipulator/__init__.py
@@ -3,7 +3,7 @@
 """
 NAME
     JSONManipulator.
-#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
 DESCRIPTION
     A Python library to manipulate objects in JSON files.
 
